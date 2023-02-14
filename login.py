11b6c690be05dc29669cec102b9f9e5735650fe4
João Jordan <joaojordan.deven@gmail.com>
2023-02-14 11:34:12 -0300

    centralizando cabeçalho

--- login.py
+++ login.py
@@ -1,4 +1,4 @@
-print('CADASTRADO DE CLIENTES')
+print(f'CADASTRADO DE CLIENTES'.center(30))
 nome = str(input('Digite seu nome: '))
 login = str(input('Entre com seu login: '))
 senha = str(input('Crie uma senha comum: '))
